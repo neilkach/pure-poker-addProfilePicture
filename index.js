5944197a79c7d6c4c893af66e371914ad5996c53
neilk <neilkach@gmail.com>
2024-04-25 16:01:17 +0200

    debug

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const s3 = new AWS.S3();
 
 exports.handler = async (event) => {
     const requestOrigin = event.headers ? event.headers.origin : "*";
-    
+
     try {
         // Retrieve the image data from the event
         const imageBuffer = Buffer.from(event.image, 'base64');
@@ -11,6 +11,7 @@ exports.handler = async (event) => {
         // Define the S3 bucket and key (file path) where you want to store the image
         const bucketName = 'pure-poker-profile-pics';
         const key = event.key; // Change this to your desired file path and format
+        console.log(key)
 
         // Upload the image to S3
         await s3.putObject({
@@ -19,6 +20,8 @@ exports.handler = async (event) => {
             Body: imageBuffer,
             ContentType: 'image/jpeg' // Change this according to your image format
         }).promise();
+
+        console.log("Image added to S3 successfully");
 
         return {
             statusCode: 200,
