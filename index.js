4a8518e5ed1a6176f34fb1e011e14e30ea675fea
neilk <neilkach@gmail.com>
2024-04-25 03:02:27 +0200

    testing codebuild

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
 
-//testing codebuild 
-const x = 1;
 exports.handler = async (event) => {
     try {
         // Retrieve the image data from the event
