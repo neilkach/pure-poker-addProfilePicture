dd71f81da8f29952747ca85b20df081aab74269b
neilk <neilkach@gmail.com>
2024-04-26 01:18:24 +0200

    updating users table

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
+const mysql = require('mysql');
 
 exports.handler = async (event) => {
     const requestOrigin = event.headers ? event.headers.origin : "*";
@@ -10,7 +11,7 @@ exports.handler = async (event) => {
 
         // Define the S3 bucket and key (file path) where you want to store the image
         const bucketName = 'pure-poker-profile-pics';
-        const key = event.params.key;
+        const key = event.params.username + '.jpeg';
 
         // Upload the image to S3
         await s3.putObject({
@@ -19,6 +20,8 @@ exports.handler = async (event) => {
             Body: imageBuffer,
             ContentType: 'image/jpeg' // Change this according to your image format
         }).promise();
+
+        await updateProfilePic(event.params.username, key);
 
         return {
             statusCode: 200,
@@ -42,3 +45,33 @@ exports.handler = async (event) => {
         };
     }
 };
+
+const updateProfilePic = async(username, link) => {
+    return new Promise((resolve, reject) => {
+        const connection = mysql.createConnection({
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME
+        });
+
+        connection.connect(err => {
+            if (err) {
+                console.error('Database connection failed:', err);
+                reject(new Error('Database connection failed', host, user, password, databse));
+                return;
+            }
+
+            const updateQuery = 'UPDATE users SET profile_pic_link = ? WHERE username = ?'
+            
+            connection.query(updateQuery, [link, username], (err) => {
+                connection.end();
+                if (err) {
+                    reject(new Error(`Failed to update profile_pic_link. Error: ${err.message}`));
+                    return;
+                }
+                resolve(true);
+            });
+        })
+    })
+}
